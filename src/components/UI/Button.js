3090a7888f389ae05731d724d8cc940bb1a0a8c6
eamonn <e.smyth75@gmail.com>
2022-04-06 21:59:51 +0100

    Apply settings and add comments to the Button.js component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-import butt from './Button.module.css';
+import classes from "./Button.module.css";
 
 const Button = (props) => {
-    return <button type="submit" className={`${butt.myButton} ${props.myStyle}`} >{props.children}</button>
-}
+  return (
+    <button
+      // Set the button to be the type of what is required at the button
+      type={props.type || 'button'}
+      // we want the button using this to handle the click as they req
+      onClick={props.onClick}
+      // classname will be both the default styles, with overiding from the styles applied to the button being used.
+      className={`${classes.button} ${props.myClassName}`}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
