2ff20256a6aae18068aea3e805260010ed2fa8a7
eamonn <e.smyth75@gmail.com>
2022-04-08 22:55:05 +0100

    Added useState to monitor and change username

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -8,18 +8,27 @@ import classes from './AddUser.module.css'
 
 
 const AddUser = (props) => {
+
+  // initialise useState - each time this code is run returns an array with 2 elements
+  // we store them here
+  const [enteredUsername, setEnteredUsername] = useState('');
+
+  // Our Add user from form handler
   const addUserHandler = (event) => {
       event.preventDefault();
   };
+
+  // Our State handler for Username
+  const changeUsernameHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+
   return (
     <Card myClassName={classes.user}>
-      <form className='box'>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
-        <label htmlFor="firstname">First name</label>
-        <input id="firstname" type="text"></input>
-        <label htmlFor="lastname">Last name</label>
-        <input id="lastname" type="text"></input>
+        <input id="username" type="text" onChange={changeUsernameHandler}></input>
         <label htmlFor="age">Age (In Years)</label>
         <input id="age" type="number"></input>
         <label htmlFor="email">Email</label>
